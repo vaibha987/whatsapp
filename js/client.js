4f925900674a8d145fcca6e9c99bb3374734729f
Rishabh <rishabhjajoriya27@gmail.com>
2024-05-29 17:56:10 +0530

    jabroni

--- js/client.js
+++ js/client.js
@@ -3,7 +3,18 @@ const socket = io('http://localhost:8000')
 const form = document.getElementById('send-container');
 const message = document.getElementById('messageInp');
 
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
 const messageContainer = document.querySelector('.container');
 const Name = prompt("Enter your name to join");
 socket.emit('new-user-joined',Name);
 
+socket.on('user-joined', data=>{
+
+})
