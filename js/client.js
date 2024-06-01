13531d6f2357f82ac3611be787074dff056aee90
Rishabh <rishabhjajoriya27@gmail.com>
2024-06-01 11:43:07 +0530

    spaces

--- js/client.js
+++ js/client.js
@@ -3,7 +3,7 @@ const socket = io('http://localhost:8000')
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 
-const messageContainer = document.querySelector('.container');
+const messageContainer=document.querySelector('.container');
 
 
 
