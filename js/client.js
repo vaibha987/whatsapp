92052243d2e588c642c3c0e037e2f1d2aadc2486
Rishabh <rishabhjajoriya27@gmail.com>
2024-06-04 00:18:18 +0530

    space

--- js/client.js
+++ js/client.js
@@ -30,7 +30,7 @@ const name = prompt("Enter your name to join");
 socket.emit('new-user-joined',name);
 
 socket.on('user-joined', name=>{
-    append(`${name} joined the chat`, 'right');
+    append(`${name} joined the chat`, 'right' );
 })
 
 
